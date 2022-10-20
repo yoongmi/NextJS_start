064c730ab91a1ec6374fe9f51bb2e204e6adebc5
yoongmi <78575434+yoongmi@users.noreply.github.com>
2022-10-20 23:12:55 +0900

    redirect, rewrite

--- nextjs-intro/next.config.js
+++ nextjs-intro/next.config.js
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = process.env.API_KEY;
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  async redirects() {
+    // url이 변경됨
+    return [
+      {
+        source: "/contact",
+        destination: "https://nomadcoders.co",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    // url을 변경되지 않음.
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
