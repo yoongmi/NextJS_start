070fd2b9bdaa613b6b3483d2428ff3b03dc49596
yoongmi <78575434+yoongmi@users.noreply.github.com>
2022-10-21 16:27:46 +0900

    detail 추가

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,14 +1,37 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
 export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(
+      { pathname: `/movies/${id}`, query: { title: title } },
+      `/movies/${id}`
+    );
+  };
   return (
     <div className="container">
       <Seo title="Home" />
       {results.map((movie) => (
-        <div className="movie" key={movie.id}>
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+          <h4>
+            <Link
+              href={{
+                pathname: `/movies/${movie.id}`,
+                query: { title: movie.original_title },
+              }}
+              as={`/movie/${movie.id}`}
+            >
+              <a>{movie.original_title}</a>
+            </Link>
+          </h4>
         </div>
       ))}
       <style jsx>{`
@@ -40,7 +63,6 @@ export default function Home({ results }) {
 }
 
 export async function getServerSideProps() {
-  // function 이름 바꾸면 안됨.
   // 이코드는 server쪽에서만 작동.
   // 여기 안의 코드는 client 에서 볼수 없음.
 
@@ -48,6 +70,7 @@ export async function getServerSideProps() {
   const { results } = await (
     await fetch(`http://localhost:3000/api/movies`)
   ).json();
+
   return {
     props: {
       results,
